06a7d75b9f55f4ab8c6385cff2cfcd17ac12766b
D3SP3R4D0S <90773781+D3SP3R4D0S@users.noreply.github.com>
2024-02-05 01:07:44 +0900

    [feat] add database connection

---
+++ databases/basedb.py
@@ -0,0 +1,22 @@
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+account = config["account"]
+
+DB_URL = f'mysql+pymysql://{account["db_user"]}:{account["db_password"]}@{account["db_host"]}:{account["db_port"]}/{account["db_scheme"]}'
+
+class engineconn:
+    def __init__(self):
+        self.engine = create_engine(DB_URL, pool_recycle = 500)
+
+    def sessionmaker(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        return session
+
+    def connection(self):
+        conn = self.engine.connect()
+        return conn
