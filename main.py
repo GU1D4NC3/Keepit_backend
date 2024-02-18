9b4333b58d17657d0beb0dce810b2f97c1f14407
D3SP3R4D0S <90773781+D3SP3R4D0S@users.noreply.github.com>
2024-02-19 01:48:52 +0900

    [feat] apply hospital router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import google, user, diary, vision, newsnqz
+from routers import google, user, diary, vision, newsnqz, hospital
 import os
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'googlekey.json'
@@ -24,6 +24,10 @@ tags_metadata = [
     {
         "name": "News and quiz",
         "description": "News data api"
+    },
+    {
+        "name": "Hospital",
+        "deacription": "Hospital CRUD"
     }
 ]
 
@@ -48,6 +52,7 @@ app.include_router(user.router, prefix="/user", tags=["User management"])
 app.include_router(diary.router, prefix="/diary", tags=["Diary"])
 app.include_router(vision.router, prefix="/vision", tags=["Google Vision"])
 app.include_router(newsnqz.router, prefix="/news", tags=["News and quiz"])
+app.include_router(hospital.router, prefix="/hospital", tags=["Hospital"])
 
 
 if __name__ == "__main__":
